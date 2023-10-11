1797fcaa1fc51c701571d9d031836e4e8db44eea
tristan <thuckabee.work@gmail.com>
2023-10-11 17:54:20 -0500

    phase 1 done

--- social.js
+++ social.js
@@ -8,23 +8,43 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here
+    this.currentID += 1;
+    let newUser = {
+      'id': this.currentID,
+      'name': name
+    }
+    this.users[newUser.id] = newUser;
+    this.follows[this.currentID] = new Set();
+    return this.currentID;
   }
 
   getUser(userID) {
-    // Your code here
+    return this.users[userID] ? this.users[userID] : null;
   }
 
   follow(userID1, userID2) {
-    // Your code here
+    let follows = this.follows[userID1];
+    if (follows) {
+      if (follows.has(userID2))   return false;
+      if (!this.getUser(userID2)) return false;
+      this.follows[userID1].add(userID2);
+      return true;
+    }
+    return false;
   }
 
   getFollows(userID) {
-    // Your code here
+    return this.follows[userID];
   }
 
   getFollowers(userID) {
-    // Your code here
+    let followers = new Set();
+    for (let f in this.follows) {
+      if (this.follows[f].has(userID)) {
+        followers.add(Number(f));
+      }
+    }
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
